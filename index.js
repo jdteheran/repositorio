a03bb022fd1dfa84789312e4d4631ee109c46dcc
Juan Teheran <jdteheran@uninorte.edu.co>
2021-11-16 18:47:18 -0500

    Este es mi segundo commit de mi algoritmo

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hola grupo de programacion numero 16');
+
+let numero = 17
+
+function esPrimo(num) {
+    let contDivisores = 0
+    for (let i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            contDivisores = contDivisores + 1
+        }
+    }
+}
