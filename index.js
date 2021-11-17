1c5dd6f6b5f183901fc80db69aca7c587cdb60b8
Juan Teheran <jdteheran@uninorte.edu.co>
2021-11-16 19:47:59 -0500

    commit desde master despues de las ramas

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(esPrimo(numero));
 function esPrimo(num) {
     let contDivisores = 0
     for (let i = 1; i <= num; i++) {
-        if (num % i == 0) {
+        if (2 * num % i == 0) {
             contDivisores = contDivisores + 1
         }
     }
