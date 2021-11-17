60c21fbaa3230a35f6340a469d45ec70e6c98648
Juan Teheran <jdteheran@uninorte.edu.co>
2021-11-16 19:41:59 -0500

    primer commit desde dev2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(esPrimo(numero));
 function esPrimo(num) {
     let contDivisores = 0
     for (let i = 1; i <= num; i++) {
-        if (num % i == 0) {
+        if (num % i == 0 && true) {
             contDivisores = contDivisores + 1
         }
     }
